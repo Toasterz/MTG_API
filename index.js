358c15a4fd1fd669bffeb35fcaac755e484be7fe
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-07-06 19:07:54 -0400

    finished setting up all base routes

--- index.js
+++ index.js
@@ -21,6 +21,51 @@ server.get('/cards', function(req, res)
   res.send(result);
 });
 
+server.get('/cards/id', function (req, res)
+{
+  var todo = db.get('cards')
+            .find({id: req.params.id})
+            .value();
+res.send(todo);
+});
+
+server.post('/cards', function(req, res)
+{
+  var card = {
+    id: uuid.v4(),
+    name: req.body.name,
+    CMC: req.body.cmc,
+    price: req.body.price,
+  };
+  var result = db.get('cards')
+                  .push(card)
+                  .last()
+                  .value();
+res.send(result);
+});
+
+server.put('/cards/id', function(req, res)
+{
+  var updatedCardInfo = {
+    name: req.body.name,
+    CMC: req.body.CMC,
+    price: req.body.price,
+  };
+  var updatedInfo = db.get('cards')
+                      .find({id: req.params.id})
+                      .assign(updatedCardInfo)
+                      .value();
+  res.send(updatedInfo);
+});
+
+server.delete('/cards/id', function(req, res)
+{
+  var card = db.get('cards')
+              .remove({id: req.params.id})
+              .value()
+  res.send(card);
+})
+
 server.listen(port, function()
 {
   console.log("Now listening on port " + port);
